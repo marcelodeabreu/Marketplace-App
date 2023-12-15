5a1dd033596f34d2a5a19975be874ed90547cb56
Marcelo de Abreu <marcelodeabreu@Marcelos-MacBook-Pro.local>
2023-12-15 03:50:20 -0300

    created tableview for market and inventory

--- viewcodeAppExample/Controllers/InventoryViewController.swift
+++ viewcodeAppExample/Controllers/InventoryViewController.swift
@@ -17,11 +17,21 @@
 import UIKit
 
 class InventoryViewController: UIViewController {
+    
+    private let inventoryFeedTable: UITableView = {
+           let table = UITableView(frame: .zero, style: .grouped)
+           table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+           return table
+       }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(inventoryFeedTable)
 
         configureNavBar()
+        
+        inventoryFeedTable.delegate = self
+        inventoryFeedTable.dataSource = self
     }
     
     private func configureNavBar() {
@@ -40,7 +50,39 @@ class InventoryViewController: UIViewController {
         
     }
     
+    override func viewDidLayoutSubviews() {
+           super.viewDidLayoutSubviews()
+           inventoryFeedTable.frame = view.bounds
+       }
+}
 
+extension InventoryViewController: UITableViewDelegate, UITableViewDataSource {
     
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    
+    
 }
